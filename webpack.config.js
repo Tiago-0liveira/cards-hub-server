9e55577cf8dfc0f363e5303867baba682044f577
Tiago-0liveira <tfgoliveira@gmail.com>
2025-03-20 18:35:58 +0000

    (fix): full build system makeover (now using webpack with nodemon)

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+	entry: './src/server.ts', // Your main entry file
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	target: "node",
+	externals: [nodeExternals()], // ✅ Ignore all `node_modules`
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	devServer: {
+		hot: true,
+		port: 3000,
+		watchFiles: ["src/**/*.ts"]
+	},
+};
